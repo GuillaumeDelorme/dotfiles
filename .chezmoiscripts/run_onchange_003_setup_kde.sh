3e10f48e181bb37b3ca71160623279c69b590490
Guillaume Delorme <guillaume.delorme.fr@gmail.com>
2023-09-18 19:02:12 +0200

    💄 Set KDE icon theme

--- .chezmoiscripts/run_onchange_003_setup_kde.sh
+++ .chezmoiscripts/run_onchange_003_setup_kde.sh
@@ -2,3 +2,6 @@
 
 # Dolphin : show full path
 kwriteconfig5 --file ~/.config/dolphinrc --group General --key ShowFullPath "true"
+
+# Theme
+kwriteconfig5 --file ~/.config/kdeglobals --group Icons --key Theme "kora"
